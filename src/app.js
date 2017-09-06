111c4f9b6b6cf11ccba67eb09006418f788d86d0
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-09-06 17:15:41 +1000

    add code for booklist and booklist components

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BookList from './components/book_list';
+import BookNotes from './components/book_notes';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      books: []
+    };
+
+    var books = JSON.parse(localStorage.getItem('books')) || [];
+
+    this.setState({
+      books: books
+    });
+  }
   render() {
     return (
       <div>
-        <BookList />
-        <BookNotes />
+        <BookList
+          books={this.state.books}
+          onBookSelect={selectedBook => this.setState({selectedBook}) }/>
+        <BookNotes book={this.state.selectedBook}/>
       </div>;
     )
   }
 }
+
+
+
+//localStorage.setItem('books', JSON.stringify(todos));
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
--- src/components/book_list_item.js
+++ src/components/book_list_item.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const BookListItem = ({book, onBookSelect} => {
+  return (
+    <li onClick={() => onBookSelect(book)} className="list-group-item">
+      <div className="main">
+        <div className="title">{book.title}</div>
+        <div className="snippet">{book.snippet}</div>
+      </div>
+    </li>
+  );
+});
+
+export default BookListItem;
